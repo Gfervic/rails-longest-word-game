063ef59bccfd9c4e40f6f614048156f3ec587879
gon.fervic <gon.fervic@gmail.com>
2017-11-06 16:14:00 +0000

    added wordscontroller, game and scorte actions and some styling

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,33 @@
+require "open-uri"
+
+class WordsController < ApplicationController
+  VOWELS = %w(A E I O U)
+
+  def game
+    # random new grid of letters
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) {(("A".."Z").to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+    # Not able to comput & display score as in the ruby exercise - ? for help
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+end
